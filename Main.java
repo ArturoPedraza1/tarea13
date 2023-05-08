6fba816872c2aa9290a99e25aad2d280744364ad
ArturoPedraza1 <125940774+ArturoPedraza1@users.noreply.github.com>
2023-05-07 19:15:44 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+import ico.VentanaSwing;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        VentanaSwing ventanaswing = new VentanaSwing("Centigrados a farenheid");
+
+    }
+}
